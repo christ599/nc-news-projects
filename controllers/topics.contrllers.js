66bd4ef3aea33d133ee380b95c2687d9c1e678e2
christian kikep nguemou <christiankips@yahoo.fr>
2025-03-13 10:19:02 +0000

    task 4

--- controllers/topics.contrllers.js
+++ controllers/topics.contrllers.js
@@ -1,7 +1,6 @@
 const {selectTopics} = require("../models/topics.models")
 
 exports.getTopics = (request, response)=>{
-    console.log("hello from the controllers")
     selectTopics().then((topics)=>{
         response.status(200).send({topics:topics})
 
--- models/topics.models.js
+++ models/topics.models.js
@@ -1,6 +1,5 @@
 const db = require("../db/connection");
 exports.selectTopics = ()=>{
-    console.log("hi from the models")
     return db.query(`SELECT * FROM topics`).then(({rows})=>{
            return rows
     })
